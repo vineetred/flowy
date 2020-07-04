a14fe8c63c1cfad6828c0a6098c558f128ad3115
vineetred <vineetreddy@live.com>
2020-07-04 21:29:59 +0530

    Demo added

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
         // The times are set by themselves
         // Just supply the path and the TOML file is generated
-        flowy::generate_config("/home/vineet/Downloads/walls").unwrap();
+        flowy::generate_config("/home/vineet/Downloads/").unwrap();
         // Runs forever
         flowy::set_times();
 
